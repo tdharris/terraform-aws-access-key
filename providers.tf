c954b558c5423e95a0c0e8776807f9ab2001aabb
Tyler Harris <tyler.harris@microfocus.com>
2021-09-22 10:24:50 -0600

    Add tfenv and rely on provider env vars

--- providers.tf
+++ providers.tf
@@ -9,7 +9,6 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
-}
+# Provide credentials with environment variables. 
+# E.g. AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION
+provider "aws" {}
